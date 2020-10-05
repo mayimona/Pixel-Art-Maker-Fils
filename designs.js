4017310dafb3277b689db551f10992bc918f5b60
Fils Mavungu <fils@digitalsolutionfoundry.co.za>
2020-10-05 15:51:50 +0200

    first commit

---
+++ designs.js
@@ -0,0 +1,38 @@
+// Select color input
+// Select size input
+
+var gridHeight = document.getElementById('inputHeight');
+var gridWidth = document.getElementById('inputWidth');
+var colrvalue = document.getElementById('colorPicker');
+var canvas = document.getElementById('pixelCanvas');
+let selectedColor = '';
+
+// When size is submitted by the user, call makeGrid()
+
+function makeGrid() {
+    clearGrid();
+    //console.log('test');
+    console.log(gridWidth.value);
+    console.log(canvas.value);
+    // Your code goes here!
+    selectedColor = colrvalue.value;
+
+    for (let r = 0; r < gridHeight.value; r++) {
+        var row = canvas.insertRow(r);
+        for (let c = 0; c < gridWidth.value; c++) {
+            var cell = row.insertCell(c);
+            cell.addEventListner("click", onCellClicked(selectedColor, cell));
+        }
+    }
+
+};
+
+function clearGrid() {
+    while (canvas.firstChild)
+        canvas.removeChild(canvas.firstChild);
+}
+
+function onCellClicked(colorToChange, cell) {
+    cell.setAttribute("style", `background-color:${colorToChange}`);
+
+}
