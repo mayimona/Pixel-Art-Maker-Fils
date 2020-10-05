27b398c13bddd8dbf3dc0564c5d579b907db4f25
Fils Mavungu <fils@digitalsolutionfoundry.co.za>
2020-10-05 18:30:14 +0200

    added clear grid loop and check for data in canvas and allow multiple colors

--- designs.js
+++ designs.js
@@ -3,37 +3,56 @@
 
 var gridHeight = document.getElementById('inputHeight');
 var gridWidth = document.getElementById('inputWidth');
-var colrvalue = document.getElementById('colorPicker');
+var colorvalue = document.getElementById('colorPicker');
 var canvas = document.getElementById('pixelCanvas');
+var formInfo = document.getElementById('sizePicker'); 
 let selectedColor = '';
+let canvasHasData = false;
+
+formInfo.onsubmit = function(event){
+    event.preventDefault();
+    makeGrid();
+};
+
+//allow multiple colors
+colorvalue.onchange = function(){
+    selectedColor = '';
+    selectedColor = colorvalue.value;
+};
 
 // When size is submitted by the user, call makeGrid()
-
 function makeGrid() {
+ 
     clearGrid();
     //console.log('test');
     console.log(gridWidth.value);
     // Your code goes here!
-    selectedColor = colrvalue.value;
+    selectedColor = colorvalue.value;
     console.log(selectedColor);
-
 
     for (let r = 0; r < gridHeight.value; r++) {
         var row = canvas.insertRow(r);
+        canvasHasData = true;
         for (let c = 0; c < gridWidth.value; c++) {
             var cell = row.insertCell(c);
-           // cell.addEventListner("click", onCellClicked(selectedColor, cell));
+            cell.addEventListener("click", onCellClicked);
         }
     }
 
 };
 
 function clearGrid() {
-    while (canvas.firstChild)
-        canvas.removeChild(canvas.firstChild);
+    let r = canvas.rows.length - 1;
+    canvasHasData = canvas.rows.length > 0 ? true : false
+    while (r >= 0)
+    {
+        if(r >=0 && canvasHasData == true)
+        canvas.deleteRow(r);
+        r--;
+    }
+    canvasHasData = false;
 }
 
-function onCellClicked(colorToChange, cell) {
-    cell.setAttribute("style", `background-color:${colorToChange}`);
-
+function onCellClicked(){
+    this.setAttribute("style", `background-color:${selectedColor}`);
 }
